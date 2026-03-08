feb2457670f181d807e026d509a6c5361b685aa1
Yulia <yulia.shevtsova2005@gmail.com>
2026-03-08 22:03:14 +0600

    Fixed the main application screen. Changed the design. Implemented a click on the buttons. updated the logic of the _on_exit_pressed() handler

--- main_menu.gd
+++ main_menu.gd
@@ -1,19 +1,24 @@
 extends Control
 
-## Главное меню с кнопками Play и Settings
+## Главное меню. Нажатия обрабатываем по зонам поверх готового арт-меню.
 
-@onready var play_button: Button = $VBoxContainer/PlayButton
-@onready var settings_button: Button = $VBoxContainer/SettingsButton
+@onready var play_area: Button = $PlayArea
+@onready var settings_area: Button = $SettingsArea
+@onready var exit_area: Button = $ExitArea
 
 var game_scene = preload("res://game.tscn")
 var settings_scene = preload("res://settings.tscn")
 
 func _ready() -> void:
-	play_button.pressed.connect(_on_play_pressed)
-	settings_button.pressed.connect(_on_settings_pressed)
+	play_area.pressed.connect(_on_play_pressed)
+	settings_area.pressed.connect(_on_settings_pressed)
+	exit_area.pressed.connect(_on_exit_pressed)
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_packed(game_scene)
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_packed(settings_scene)
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
