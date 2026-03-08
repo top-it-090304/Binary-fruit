7da1281f6b91c8124d79809318d1529207b9f261
Yulia <yulia.shevtsova2005@gmail.com>
2026-03-08 22:12:48 +0600

    fixed node hierarchy, added secure GameManager checks

--- settings.gd
+++ settings.gd
@@ -2,30 +2,33 @@ extends Control
 
 ## Экран настроек
 
-@onready var sound_checkbox: CheckBox = $VBoxContainer/SoundCheckBox
-@onready var music_checkbox: CheckBox = $VBoxContainer/MusicCheckBox
-@onready var back_button: Button = $VBoxContainer/BackButton
+@onready var sound_checkbox: CheckBox = $Panel/VBoxContainer/SoundCheckBox
+@onready var music_checkbox: CheckBox = $Panel/VBoxContainer/MusicCheckBox
+@onready var back_button: Button = $Panel/VBoxContainer/BackButton
 
 var main_menu_scene = preload("res://main_menu.tscn")
 
 func _ready() -> void:
-	var game_manager = get_node("/root/GameManager")
-	sound_checkbox.button_pressed = game_manager.sound_enabled
-	music_checkbox.button_pressed = game_manager.music_enabled
+	var game_manager = get_node_or_null("/root/GameManager")
+	if game_manager:
+		sound_checkbox.button_pressed = game_manager.sound_enabled
+		music_checkbox.button_pressed = game_manager.music_enabled
 	
 	sound_checkbox.toggled.connect(_on_sound_toggled)
 	music_checkbox.toggled.connect(_on_music_toggled)
 	back_button.pressed.connect(_on_back_pressed)
 
 func _on_sound_toggled(button_pressed: bool) -> void:
-	var game_manager = get_node("/root/GameManager")
-	game_manager.sound_enabled = button_pressed
-	game_manager.save_settings()
+	var game_manager = get_node_or_null("/root/GameManager")
+	if game_manager:
+		game_manager.sound_enabled = button_pressed
+		game_manager.save_settings()
 
 func _on_music_toggled(button_pressed: bool) -> void:
-	var game_manager = get_node("/root/GameManager")
-	game_manager.music_enabled = button_pressed
-	game_manager.save_settings()
+	var game_manager = get_node_or_null("/root/GameManager")
+	if game_manager:
+		game_manager.music_enabled = button_pressed
+		game_manager.save_settings()
 
 func _on_back_pressed() -> void:
 	# Возвращаемся на предыдущий экран (главное меню)
