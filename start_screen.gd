8da6c3baa545c754035a5cd5e23bf09581a4db13
Yulia <yulia.shevtsova2005@gmail.com>
2026-03-13 10:32:28 +0600

    Changed the design of the home screen

--- start_screen.gd
+++ start_screen.gd
@@ -1,19 +1,10 @@
 extends Control
 
-## Экран запуска с анимацией увеличения фрукта
-
-@onready var fruit_sprite: Sprite2D = $FruitSprite
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var timer: Timer = $Timer
 
 var main_menu_scene = preload("res://main_menu.tscn")
 
 func _ready() -> void:
-	fruit_sprite.scale = Vector2(0.1, 0.1)
-	fruit_sprite.modulate.a = 0.0
-	
-	animation_player.play("fruit_grow")
-	
 	timer.wait_time = 2.5
 	timer.timeout.connect(_on_timer_timeout)
 	timer.start()
