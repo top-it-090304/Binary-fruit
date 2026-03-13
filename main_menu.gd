1b91fd0829c3d5027207c484e33aa3501d9111b9
Yulia <yulia.shevtsova2005@gmail.com>
2026-03-13 10:35:15 +0600

    Сhanged the design of the main screen

--- main_menu.gd
+++ main_menu.gd
@@ -4,7 +4,6 @@ extends Control
 
 @onready var play_area: Button = $PlayArea
 @onready var settings_area: Button = $SettingsArea
-@onready var exit_area: Button = $ExitArea
 
 var game_scene = preload("res://game.tscn")
 var settings_scene = preload("res://settings.tscn")
@@ -12,13 +11,9 @@ var settings_scene = preload("res://settings.tscn")
 func _ready() -> void:
 	play_area.pressed.connect(_on_play_pressed)
 	settings_area.pressed.connect(_on_settings_pressed)
-	exit_area.pressed.connect(_on_exit_pressed)
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_packed(game_scene)
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_packed(settings_scene)
-
-func _on_exit_pressed() -> void:
-	get_tree().quit()
